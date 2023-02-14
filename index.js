5600fea0ca61e2c308c151602e4f9d24585425ee
Alx-Assis <alxassis@gmail.com>
2023-02-13 23:25:50 -0300

    modif.  file index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const server = express()
 
 
 const port = process.env.PORT || 3333
+
+server.get("/",(req,res)=>{
+  return res.send("pagina inicial")
+})
 server.listen(port, ()=>{
   console.log(`listen in port : ${port}`)
 })
